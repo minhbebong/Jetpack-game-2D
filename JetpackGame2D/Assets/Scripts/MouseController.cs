ab1923b8e8d5bfb583cc482ea1fdd1548751b7d3
huypnd <phanhuycode@gmail.com>
2023-06-14 11:00:25 +0700

    MouseController

---
+++ JetpackGame2D/Assets/Scripts/MouseController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public float jetpackForce = 75f;
+    private Rigidbody2D playerbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+        if (jetpackActive)
+        {
+            playerbody.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+}
