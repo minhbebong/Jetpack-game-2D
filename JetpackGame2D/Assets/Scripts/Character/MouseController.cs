d8926772a9825cacb365a491e8e1b24da0167a47
minhndhe151002 <hichic375@gmail.com>
2023-06-23 15:07:40 +0700

    Killing the Mouse

--- JetpackGame2D/Assets/Scripts/Character/MouseController.cs
+++ JetpackGame2D/Assets/Scripts/Character/MouseController.cs
@@ -8,6 +8,18 @@ public class MouseController : MonoBehaviour
     public float forwardMovementSpeed = 3.0f;
     public float jetpackForce = 75f;
     private Rigidbody2D playerbody;
+    private bool isDead = false;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HitByLaser(collider);
+    }
+
+    void HitByLaser(Collider2D laserCollider)
+    {
+        isDead = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +39,35 @@ public class MouseController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 pos = transform.position;
-        pos.x += forwardMovementSpeed * Time.deltaTime;
-        transform.position = pos;
+        //Vector3 pos = transform.position;
+        //pos.x += forwardMovementSpeed * Time.deltaTime;
+        //transform.position = pos;
+        bool jetpackActive = Input.GetButton("Fire1");
+        jetpackActive = jetpackActive && !isDead;
+
+        if (jetpackActive)
+        {
+            playerbody.AddForce(new Vector2(0, jetpackForce));
+        }
+
+        if (!isDead)
+        {
+            Vector2 newVelocity = playerbody.velocity;
+            newVelocity.x = forwardMovementSpeed;
+            playerbody.velocity = newVelocity;
+        }
+        
+        UpdateGroundedStatus();
+        AdjustJetpack(jetpackActive);
+    }
+
+    void UpdateGroundedStatus()
+    {
+        // TODO: Implement your logic for updating grounded status here
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        // TODO: Implement your logic for adjusting the jetpack here
     }
 }
